3192615307670e59fb8e6f259973306bd5c0cffd
Attila Levai <attila.levai@sedo.com>
2016-03-03 10:22:50 +0100

    Added list with required tools and xrandr config

---
+++ .screenlayout/laptop-3-screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 3840x312 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1-2 --off --output DP1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
